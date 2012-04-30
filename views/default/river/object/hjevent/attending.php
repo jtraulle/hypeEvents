5d84973f78ec45feb3351e6b28383fc6178d6645
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2012-04-30 12:50:52 +0200

    attending fix

--- views/default/river/object/hjevent/attending.php
+++ views/default/river/object/hjevent/attending.php
@@ -1,15 +1,26 @@
 <?php
+
 /**
  * Default Form Submission River
  */
-
+$subject = $vars['item']->getSubjectEntity();
 $object = $vars['item']->getObjectEntity();
 $excerpt = strip_tags($object->excerpt);
 $excerpt = elgg_get_excerpt($excerpt);
 
-$attendees = $object->getRSVPs('attending', 5);
+$attendees = $object->getRSVPs('attending', 10);
+if (sizeof($attendees) > 1) {
+	$attachment .= '<div>' . elgg_echo('hj:events:guestlist') . '</div>';
+	$attachment .= '<ul class="elgg-gallery">';
+	foreach ($attendees as $attendee) {
+		if ($subject->guid !== $attendee->guid) {
+			$attachment .= '<li class="elgg-list-entity">' . elgg_view_entity_icon($attendee, 'small');
+		}
+	}
+	$attachment .= '</ul>';
+}
 echo elgg_view('river/item', array(
 	'item' => $vars['item'],
 	'message' => $excerpt,
-    'attachments' => elgg_echo('hj:events:guestlist') . '<br />' . elgg_view_entity_list($attendees, array('list_type' => 'gallery', 'size' => 'small'))
+	'attachments' => $attachment
 ));
